38403a50271c1b1d2cf7da8156f407646909f1e5
Sahil Thakur <mesahilthakur@gmail.com>
2023-09-09 21:46:26 +0530

    some changes

--- 01usestatehook/src/App.jsx
+++ 01usestatehook/src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 
 function App() {
 
-  const [showPassword, setShowPassword] = useState(false);
+  const [ showPassword, setShowPassword] = useState(false);
 
   return (
     <>
       <h1>Use of useState Hook</h1>
       <h2>Show | Hide password</h2>
-      <input type= {showPassword ? "text" : "password"} />
+      <input type={showPassword ? "text" : "password"} />
       <button onClick={()=>{setShowPassword(!showPassword)}}>{showPassword ? "Hide" : "Show"}</button>
     </>
   )
