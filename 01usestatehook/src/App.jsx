0f6d4066f43508b7ebd491d38e6d0ca477ccc1d3
Sahil Thakur <mesahilthakur@gmail.com>
2023-09-10 19:28:49 +0530

    clean

--- 01usestatehook/src/App.jsx
+++ 01usestatehook/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [ showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <>
-      <h1>Use of useState Hook</h1>
-      <h2>Show | Hide password</h2>
-      <input type={showPassword ? "text" : "password"} />
-      <button onClick={()=>{setShowPassword(!showPassword)}}>{showPassword ? "Hide" : "Show"}</button>
-    </>
-  )
+    <div>
+      <div className="home">
+        <h1>Use of useState Hook</h1>
+        <h2>Show | Hide password</h2>
+        <input type={showPassword ? "text" : "password"} />
+        <button
+          onClick={() => {
+            setShowPassword(!showPassword);
+          }}
+        >
+          {showPassword ? "Hide" : "Show"}
+        </button>
+      </div>
+
+      <div className="loginmodal">
+        <h2>Login</h2>
+        <h2>Password</h2>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
